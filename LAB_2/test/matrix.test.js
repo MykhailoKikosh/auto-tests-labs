689f6638ac22c0f048c69be94a916b1d2e742106
Mykhailo Kikosh <mikhail.kikosh@gmail.com>
2024-09-26 13:08:46 +0300

    Lab 2: Development using TDD approach. RED phase: added test for two matrices multiplication operation.

--- LAB_2/test/matrix.test.js
+++ LAB_2/test/matrix.test.js
@@ -23,4 +23,19 @@ describe('Matrix Operations check', () => {
         const result = matrixA.sub(matrixB);
         expect(result.data).to.deep.equal([[4, 6, 0], [2, 2, -4], [-3, -3, 0]]);
     });
+
+    it('Should multiply two matrices correctly', () => {
+        const matrixA = new Matrix([
+            [5, 8, 3],
+            [6, 7, 2],
+            [4, 5, 9]
+        ]);
+        const matrixB = new Matrix([
+            [1, 2, 3],
+            [4, 5, 6],
+            [7, 8, 9]
+        ]);
+        const result = matrixA.mul(matrixB);
+        expect(result.data).to.deep.equal([[58, 74, 90], [48, 63, 78], [87, 105, 123]]);
+    });
 });
