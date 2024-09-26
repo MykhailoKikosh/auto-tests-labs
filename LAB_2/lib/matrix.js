7fd4f55bea16dfebfaa945068aa671dfc4779bdf
Mykhailo Kikosh <mikhail.kikosh@gmail.com>
2024-09-26 13:09:39 +0300

    Lab 2: Development using TDD approach. GREEN phase: added realization for two matrices multiplication operation.

--- LAB_2/lib/matrix.js
+++ LAB_2/lib/matrix.js
@@ -24,6 +24,27 @@ class Matrix {
             row.map((val, j) => val - matrix.data[i][j])
         ));
     }
+
+    /**
+     * Multiply matrix to other matrix
+     * @param matrix
+     * @returns {Matrix}
+     */
+    mul(matrix) {
+        const result = [];
+        for (let i = 0; i < this.data.length; i++) {
+            result[i] = [];
+            for (let j = 0; j < matrix.data[0].length; j++) {
+                let sum = 0;
+                for (let k = 0; k < this.data[0].length; k++) {
+                    sum += this.data[i][k] * matrix.data[k][j];
+                }
+                result[i][j] = sum;
+            }
+        }
+
+        return new Matrix(result);
+    }
 }
 
 export { Matrix };
