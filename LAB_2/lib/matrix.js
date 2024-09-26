60a41dc960087d33745a51d63de146159daab6cb
Mykhailo Kikosh <mikhail.kikosh@gmail.com>
2024-09-26 14:06:01 +0300

    Lab 2: Development using TDD approach. GREEN phase: added functionality for two matrices division operation.

--- LAB_2/lib/matrix.js
+++ LAB_2/lib/matrix.js
@@ -37,13 +37,72 @@ class Matrix {
             for (let j = 0; j < matrix.data[0].length; j++) {
                 let sum = 0;
                 for (let k = 0; k < this.data[0].length; k++) {
-                    sum += this.data[i][k] * matrix.data[k][j];
+                    sum += Math.round(this.data[i][k] * matrix.data[k][j]);
                 }
                 result[i][j] = sum;
             }
         }
 
         return new Matrix(result);
+    }
+
+    /**
+     * Matrices division
+     * @param matrix
+     * @returns {Matrix}
+     */
+    div(matrix) {
+        const inverseMatrix = matrix.inverse();
+        return this.mul(inverseMatrix);
+    }
+
+    /**
+     * matrix inversion
+     * @returns {Matrix}
+     */
+    inverse() {
+        const n = this.data.length;
+        const augmented = this.data.map((row, i) =>
+            [...row, ...Array(n).fill(0).map((_, j) => (i === j ? 1 : 0))]
+        );
+
+        for (let i = 0; i < n; i++) {
+            let maxRow = i;
+            for (let k = i + 1; k < n; k++) {
+                if (Math.abs(augmented[k][i]) > Math.abs(augmented[maxRow][i])) {
+                    maxRow = k;
+                }
+            }
+
+            [augmented[i], augmented[maxRow]] = [augmented[maxRow], augmented[i]];
+
+            for (let k = i + 1; k < n; k++) {
+                const factor = augmented[k][i] / augmented[i][i];
+                for (let j = i; j < 2 * n; j++) {
+                    augmented[k][j] -= factor * augmented[i][j];
+                }
+            }
+        }
+
+        for (let i = n - 1; i >= 0; i--) {
+            for (let k = i - 1; k >= 0; k--) {
+                const factor = augmented[k][i] / augmented[i][i];
+                for (let j = 0; j < 2 * n; j++) {
+                    augmented[k][j] -= factor * augmented[i][j];
+                }
+            }
+        }
+
+        for (let i = 0; i < n; i++) {
+            const factor = augmented[i][i];
+            for (let j = 0; j < 2 * n; j++) {
+                augmented[i][j] /= factor;
+            }
+        }
+
+        const inverseMatrix = augmented.map(row => row.slice(n));
+
+        return new Matrix(inverseMatrix);
     }
 }
 
